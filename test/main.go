994432815297b97c6026a7692e22e5eda8f28ce7
TheKving <kevingomezhurtado@gmail.com>
2023-02-07 12:11:56 +0000

    remplace

--- test/main.go
+++ test/main.go
@@ -21,15 +21,28 @@ func main() {
 	//Exec command hostname -f to obtain the hostname of server
 	hostname, _ := exec.Command("hostname", "-f").Output()
 	//Obtain the public ip address of server
-	PUBLICIPADDRESS1, _ := exec.Command("curl", "ifconfig.co").Output()
+	PUBLICIPADDRESS1, err := exec.Command("curl", "ifconfig.co").Output()
+	/*PUBLICIPADDRESS2, _ := exec.Command("curl", "ifconfig.me").Output()
+	PUBLICIPADDRESS3, _ := exec.Command("curl icanhazip.com").Output()*/
+
+	if err != nil {
+		fmt.Println(string(PUBLICIPADDRESS1))
+		log.Fatal("Not give ip")
+	}
 
 	//Print env vars, hostname and IP (fails in work, in sv works?)
 	fmt.Println(username)
 	fmt.Println(password)
 	fmt.Println(string(hostname))
 	fmt.Println(string(PUBLICIPADDRESS1))
+	/*fmt.Println(string(PUBLICIPADDRESS2))
+	fmt.Println(string(PUBLICIPADDRESS3))*/
 
 	//to do, get public ip address from other service
 	//update the dynamic ip address to google domain service with curl or other?
 	//test in production?
 }
+
+//curl ifconfig.co
+// curl ifconfig.me
+// curl icanhazip.com
